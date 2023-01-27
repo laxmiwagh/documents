15234bedf0b9cacd2a3a8bb43b910bcd7651d16e
laxmiwagh <laxmiwagh26@gmail.com>
2023-01-27 23:08:07 +0530

    update

--- tomcat.groovy
+++ tomcat.groovy
@@ -7,7 +7,7 @@ pipeline {
             steps { 
                 sh 'sudo apt-get update -y'
                 sh 'sudo apt-get install git -y'
-                git 'https://github.com/palashgadpayle/student-ui.git'
+                git 'git@github.com:laxmiwagh/student-ui.git'
                 sh 'ls'
             }
         }
@@ -16,14 +16,6 @@ pipeline {
                 sh 'sudo apt-get update -y'
                 sh 'sudo apt-get install maven curl unzip -y'
                 sh 'mvn clean package'
-            }
-        }
-        stage('build-artifacts') {
-            steps { 
-                //sh 'curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"'
-                //sh 'unzip awscliv2.zip'
-                //sh 'sudo ./aws/install'
-                sh 'aws s3 cp **/*.war s3://tomcat-bucket-1/student-${BUILD_ID}.war'
             }
         }
         stage('tomcat-build') {
